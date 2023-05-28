937bb519f2735b8fcd577b4e64aa2327178acf47
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-28 09:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let inputs = document.getElementsByTagName("input")[0];
+let id = document.getElementById("colorSelect");
+
+let color = id.value;
+
+inputs.addEventListener("click", (color) =>{
+		id.remove(color);
+})
