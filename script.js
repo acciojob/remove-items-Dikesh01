109e8a972683cbdde50bf03fab1234c1b5cf0ee8
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-28 11:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //your JS code here. If required.
-let inputs = document.getElementsByTagName("input")[0];
-let id = document.getElementById("colorSelect");
 
-let color = id.value;
 
-inputs.addEventListener("click", (color) =>{
-		id.remove(color);
-})
+// let inputs = document.getElementsByTagName("input")[0];
+// let id = document.getElementById("colorSelect");
+
+// let color.option.innerText =;
+
+// inputs.addEventListener("click", (color) =>{
+// 		id.option.remove(color);
+// })
+
+function deleteElement() {
+     let selectElement=document.getElementById("colorSelect");
+	let selectedValue=selectElement.value;
+	let childrenList=selectElement.children;
+
+	for(let i=0;i<childrenList;i++)
+		{
+			if(selectedValue===childrenList[i].value)
+			{
+				childrenList[i].remove();
+			}
+		}
+}
