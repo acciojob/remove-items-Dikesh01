cfaca57e9cbf149f34d71025fd3f1ded037d79c4
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-05-28 15:01:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,16 +10,26 @@
 // 		id.option.remove(color);
 // })
 
-function deleteElement() {
-     let selectElement=document.getElementById("colorSelect");
-	let selectedValue=selectElement.value;
-	let childrenList=selectElement.children;
 
-	for(let i=0;i<childrenList;i++)
-		{
-			if(selectedValue===childrenList[i].value)
-			{
-				childrenList[i].remove();
-			}
+//your JS code here. If required.
+function deleteElement() {
+	let sele = document.getElementsByTagName("select")[0];
+	let val = sele.value;
+	let child = sele.children;
+	for(let i = 0;i < sele.length;i++) {
+		if(val===child[i].value) {
+		 child[i].remove()
 		}
+	}
 }
+// function deleteElement() {
+//     var selectElement = document.getElementById('colorSelect');
+//     var selectedColor = selectElement.value;
+
+//     for (var i = 0; i < selectElement.options.length; i++) {
+//         if (selectElement.options[i].value === selectedColor) {
+//             selectElement.remove(i);
+//             break;
+//         }
+//     }
+// }
